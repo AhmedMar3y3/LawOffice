71a2c3e7b55eeaf8d804ad405dee798b9e3a227f
zaynab10o <zaynabm766@gmail.com>
2025-05-19 16:12:42 +0300

    Fix sidebar navigation link for users by updating route condition

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -86,7 +86,7 @@
 
 
       <li class="nav-item">
-        <a class="nav-link {{ request()->routeIs('offices.index') ? 'active' : '' }}" href="{{ route('users') }}">
+        <a class="nav-link {{ request()->routeIs('users') ? 'active' : '' }}" href="{{ route('users') }}">
       <i class="bi bi-person-fill"></i>
           <span>العملاء</span>
         </a>
