3a4d2f8bc4c2f9dabba5cf1c1ebc5a663487eb9c
zaynab10o <zaynabm766@gmail.com>
2025-05-20 16:25:08 +0300

    Add layout and main section to makeit and users views

--- resources/views/tap/users.blade.php
+++ resources/views/tap/users.blade.php
@@ -1,1 +1,20 @@
-{{ $users }}
+@extends('layout')
+
+@section('main')
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card shadow rounded-4">
+                    <div class="card-header bg-primary text-white rounded-top-4">
+                        <h3 class="mb-0">📌 المستخدمين</h3>
+                    </div>
+                    <div class="card-body">
+                        <p class="fs-4 text-center">
+                            {{ $users }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
